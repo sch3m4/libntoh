44cec6a9e6b101c726f78fdd738674d881eecde9
Matthew Hall <mhall@mhcomputing.net>
2016-03-22 02:34:45 +0000

    ipv6defrag.c: eliminate unused ipv6_equal_tuple

--- src/ipv6defrag.c
+++ src/ipv6defrag.c
@@ -80,16 +80,6 @@ unsigned int ntoh_ipv6_get_tuple4 ( struct ip6_hdr *ip , pntoh_ipv6_tuple4_t tup
 	return NTOH_OK;
 }
 
-unsigned short ipv6_equal_tuple ( void *a , void *b )
-{
-	unsigned short ret = 0;
-
-        if ( ! memcmp ( a , (void*)&((pntoh_ipv6_flow_t)b)->ident , sizeof ( ntoh_ipv6_tuple4_t) ) )
-		ret++;
-
-	return ret;
-}
-
 pntoh_ipv6_flow_t ntoh_ipv6_find_flow ( pntoh_ipv6_session_t session , pntoh_ipv6_tuple4_t tuple4 )
 {
 	ntoh_ipv6_key_t key = 0;

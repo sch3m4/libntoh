050ff84a6d18f8265100489bf3ac666048990de6
Matthew Hall <mhall@mhcomputing.net>
2016-03-22 02:37:12 +0000

    ipv6defrag.c: switch ntoh_ipv6_count_flows to uthash

--- src/ipv6defrag.c
+++ src/ipv6defrag.c
@@ -386,16 +386,14 @@ exitp:
 unsigned int ntoh_ipv6_count_flows ( pntoh_ipv6_session_t session )
 {
 	unsigned int	ret = 0;
-	int		count;
+	unsigned int	count;
 
 	if ( !params.init )
 		return ret;
 
 	lock_access( &params.lock );
 
-        sem_getvalue ( &session->max_flows , &count );
-        ret = session->flows->table_size - count;
-//	ret = htable_count ( session->flows );
+	count = HASH_COUNT(session->flows);
 
 	unlock_access( &params.lock );
 
